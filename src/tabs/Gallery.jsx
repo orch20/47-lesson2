4a64483b0144a1ec1053ca37bbc95917591a768f
orch20 <94862153+orch20@users.noreply.github.com>
2022-08-07 15:08:46 +0300

    added axios

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -4,10 +4,27 @@ import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
 export class Gallery extends Component {
+  state = {
+    query: 'cat',
+    page: 1,
+    images: [],
+  };
+
+  componentDidUpdate(_, prevState) {
+    const { query, page } = this.state;
+    if (prevState.query !== query || prevState.page !== page) {
+      this.getImages(query, page);
+    }
+  }
+  getImages = async (query, page) => {
+    console.log(query, page);
+  };
+
   render() {
     return (
       <>
-        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        {/* <Text textAlign="center">Sorry. There are no images ... 😭</Text> */}
+        <SearchForm />
       </>
     );
   }
